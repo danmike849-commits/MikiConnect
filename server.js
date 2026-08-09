c54c528484109b1c18556348eeb8b6658cd0f849
Mika Daniel <danmike849@gmail.com>
2026-08-09 08:30:10 +0100

    Update server port binding to process.env.PORT and 0.0.0.0

--- server.js
+++ server.js
@@ -76,8 +76,9 @@ io.on('connection', (socket) => {
   });
 });
 
-// Server Listener for Local & Render Environment
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
