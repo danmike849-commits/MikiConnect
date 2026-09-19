b0e47139329253b52ed4f4fa474394c2695bec83
Mika Daniel <danmike849@gmail.com>
2026-09-19 16:55:27 +0100

    Fix server.js to require app.js directly

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
-const { start, shutdown } = require('./app');
-
-start().catch(err => {
-  console.error('Startup failed:', err.message);
-  process.exit(1);
-});
-
-process.on('SIGTERM', () => shutdown('SIGTERM').catch(() => process.exit(1)));
-process.on('SIGINT', () => shutdown('SIGINT').catch(() => process.exit(1)));
+require('./app.js');
